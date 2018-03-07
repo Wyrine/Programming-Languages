8a0e314128762f49af5daaa98a656e76fb05e9f7
Wyrine <kashahat@gmail.com>
2018-03-07 12:39:47 -0500

    wrote reduce

--- csharp_mapReduce/Map.cs
+++ csharp_mapReduce/Map.cs
@@ -7,9 +7,7 @@ public partial class MapReduce<T>
 	public delegate T Work(T x);
 	public void Map(Work w)
 	{
-        if(mCount == 0)
-            return default(T);
-        Parallel.For(1, mCount, I =>{
+        Parallel.For(0, mCount, I =>{
              mList[(int)I] = w(mList[(int)I]);
         });
 	}
--- csharp_mapReduce/Reduce.cs
+++ csharp_mapReduce/Reduce.cs
@@ -7,14 +7,14 @@ public partial class MapReduce<T>
 {
     public delegate T ReduceWork(T left, T right);
     
-   /* private async Task<T> ReduceHelper(ReduceWork rw)
-    {
-
-    }*/
-
     public T Reduce(ReduceWork rw)
     {
-       return default(T); 
+        if(mCount == 0)
+            return default(T);
+        var cur = mList[0];
+        for(int i = 1; i < mCount; i++)
+            cur = rw(cur, mList[i]);
+        return cur;
     }
 
     public async Task<T> ReduceAsync(ReduceWork rw)
